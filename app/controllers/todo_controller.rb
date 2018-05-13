5fec05a33ad15981afb7d74df805e0ca189525d1
Emily Carrasquero <emilycarrasquero3000@gmail.com>
2018-05-13 18:31:01 +0000

    Added new html view

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,27 +7,23 @@ class TodoController < ApplicationController
     def show
         # Get the number the user typed in URL
         todo_id = params[:id]
-        if todo_id == 1
-            @todo_description = "Make the curriculum"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id == '2'
-            @todo_description = "Buy workshop supplies"
-            @todo_pomodoro_estimate = 3
-        elsif todo_id == '3'
-            @todo_description = "Meet with volunteers"
-            @todo_pomodoro_estimate = 2
-        elsif todo_id = '4'
-            @todo_description = "Order food for Saturday and Sunday"
-            @todo_pomodoro_estimate = 1
-        elsif todo_id = '5'
-            @todo_description = "Check pre-work assignments"
-            @todo_pomodoro_estimate = 2
-        elsif todo_id = '6'
-            @todo_description = "Send workshop location to all the students"
-            @todo_pomodoro_estimate = 1
-        elsif todo_id = '7'
-            @todo_description = "Have a great workshop"
-            @todo_pomodoro_estimate = 0
-        end
+        
+        # Grab the todo with that id from the database
+        @todo = Todo.find_by_id(todo_id)
+        
+        # Set the variable shared with template to the values we got
+        # from the database
     end
-end 
+    
+    def new
+    end
+    
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+    
+end
